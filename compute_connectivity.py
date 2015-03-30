80220b0d3dcd03a0e9e65679ade0176c4813deac
mrahim <rahim.mehdi@gmail.com>
2015-03-30 16:15:32 +0200

    graph lasso cov

--- compute_connectivity.py
+++ compute_connectivity.py
@@ -11,9 +11,55 @@ from loader import load_dynacomp, load_dynacomp_roi_timeseries, \
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from nilearn.plotting import plot_connectome
+from sklearn import covariance
+
+def plot_connectivity_matrix(subject_id, group, pc, roi_names, suffix):
+        title = subject_id + ' - ' + group
+        # plot matrix
+        output_file = os.path.join(set_figure_base_dir('connectivity'),
+                                   '_'.join([suffix, 'matrix', group, '1', subject_id]))
+        plt.figure(figsize=(8, 8))
+        plt.imshow(pc, cmap=cm.bwr, interpolation='nearest',
+                   vmin=-1, vmax=1)
+        plt.colorbar()
+        plt.xticks(range(len(roi_names)), roi_names,
+                   rotation='vertical', fontsize=16)
+        plt.yticks(range(len(roi_names)), roi_names, fontsize=16)
+        plt.title(title, fontsize=20)
+        plt.tight_layout()
+        plt.savefig(output_file)
 
 
+def plot_connectivity_glassbrain(subject_id, group, pc, roi_coords, suffix):
+        title = subject_id + ' - ' + group
+        output_file = os.path.join(set_figure_base_dir('connectivity'),
+                                   '_'.join([suffix, 'connectome', group, '1', subject_id]))
+        plt.figure(figsize=(10, 20), dpi=90)
+        plot_connectome(pc, roi_coords, edge_threshold='85%', title=title, output_file=output_file)
 
+
+def compute_graph_lasso_covariance(subject_id, group, session='func1',
+                                   preprocessing_folder='pipeline_1',
+                                   plot=True, save=True):
+    """Returns graph lasso covariance for a subject_id
+    """
+    # load timeseries
+    ts = load_dynacomp_roi_timeseries(subject_id, session=session,
+                                      preprocessing_folder=preprocessing_folder)
+    # load rois
+    roi_names, roi_coords = load_roi_names_and_coords(subject_id)
+
+    # compute covariance
+    gl = covariance.GraphLassoCV(verbose=2)
+    gl.fit(ts)
+    pc = gl.covariance_
+    if plot:
+        plot_connectivity_matrix(subject_id, group, pc, roi_names, 'gl_covariance')
+        plot_connectivity_glassbrain(subject_id, group, pc, roi_coords, 'gl_covariance')
+
+    return pc, roi_names, roi_coords
+
+    
 def compute_pearson_connectivity(subject_id, group, session='func1',
                                  preprocessing_folder='pipeline_1',
                                  plot=True, save=True):
@@ -30,26 +76,9 @@ def compute_pearson_connectivity(subject_id, group, session='func1',
     pc = np.corrcoef(ts.T)
 
     if plot:
-        title = subject_id + ' - ' + group
-        # plot matrix
-        output_file = os.path.join(set_figure_base_dir('connectivity'),
-                                   '_'.join(['matrix', group, '1', subject_id]))
-        plt.figure(figsize=(8, 8))
-        plt.imshow(pc, cmap=cm.bwr, interpolation='nearest',
-                   vmin=-1, vmax=1)
-        #plt.colorbar()
-        plt.xticks(range(len(roi_names)), roi_names,
-                   rotation='vertical', fontsize=16)
-        plt.yticks(range(len(roi_names)), roi_names, fontsize=16)
-        plt.title(subject_id, fontsize=20)
-        plt.tight_layout()
-        plt.savefig(output_file)
-        
-        # plot connectome
-        output_file = os.path.join(set_figure_base_dir('connectivity'),
-                                   '_'.join(['connectome', group, '1', subject_id]))
-        plt.figure(figsize=(10, 20), dpi=90)
-        plot_connectome(pc, roi_coords, edge_threshold='80%', title=title, output_file=output_file)
+        plot_connectivity_matrix(subject_id, group, pc, roi_names, 'pearson_corr')
+        plot_connectivity_glassbrain(subject_id, group, pc, roi_coords, 'pearson_corr')
+
     return pc, roi_names, roi_coords
 
 
@@ -60,4 +89,5 @@ dataset = load_dynacomp()
 for i in range(len(dataset.subjects)):
     print dataset.subjects[i]
     pc, roi_names, roi_coords = compute_pearson_connectivity(dataset.subjects[i],
-                                                             dataset.group[i])
+                                                             dataset.group[i])    
+    compute_graph_lasso_covariance(dataset.subjects[i], dataset.group[i])
