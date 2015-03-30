0916e875e7ff2e78703c280a4d1d756834241ff7
mrahim <rahim.mehdi@gmail.com>
2015-03-30 17:09:02 +0200

    added group sparse covariance

--- compute_connectivity.py
+++ compute_connectivity.py
@@ -12,30 +12,66 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from nilearn.plotting import plot_connectome
 from sklearn import covariance
+from nilearn.group_sparse_covariance import GroupSparseCovarianceCV
 
-def plot_connectivity_matrix(subject_id, group, pc, roi_names, suffix):
-        title = subject_id + ' - ' + group
-        # plot matrix
-        output_file = os.path.join(set_figure_base_dir('connectivity'),
-                                   '_'.join([suffix, 'matrix', group, '1', subject_id]))
-        plt.figure(figsize=(8, 8))
-        plt.imshow(pc, cmap=cm.bwr, interpolation='nearest',
-                   vmin=-1, vmax=1)
-        plt.colorbar()
-        plt.xticks(range(len(roi_names)), roi_names,
-                   rotation='vertical', fontsize=16)
-        plt.yticks(range(len(roi_names)), roi_names, fontsize=16)
-        plt.title(title, fontsize=20)
-        plt.tight_layout()
+def plot_connectivity_matrix(subject_id, group, pc, roi_names,
+                             suffix, session='func1', save=True):
+    """Plots connectivity matrix of pc
+    """
+    title = subject_id + ' - ' + group
+    # plot matrix
+    output_file = os.path.join(set_figure_base_dir('connectivity'),
+                               '_'.join([suffix, 'matrix', group,
+                                         str(session), subject_id]))
+    plt.figure(figsize=(8, 8))
+    plt.imshow(pc, cmap=cm.bwr, interpolation='nearest',
+               vmin=-1, vmax=1)
+    plt.colorbar()
+    plt.xticks(range(len(roi_names)), roi_names,
+               rotation='vertical', fontsize=16)
+    plt.yticks(range(len(roi_names)), roi_names, fontsize=16)
+    plt.title(title, fontsize=20)
+    plt.tight_layout()
+    if save:
         plt.savefig(output_file)
 
 
-def plot_connectivity_glassbrain(subject_id, group, pc, roi_coords, suffix):
-        title = subject_id + ' - ' + group
-        output_file = os.path.join(set_figure_base_dir('connectivity'),
-                                   '_'.join([suffix, 'connectome', group, '1', subject_id]))
-        plt.figure(figsize=(10, 20), dpi=90)
-        plot_connectome(pc, roi_coords, edge_threshold='85%', title=title, output_file=output_file)
+def plot_connectivity_glassbrain(subject_id, group, pc, roi_coords,
+                                 suffix, session='func1', save=True):
+    """Plots connectome of pc
+    """
+    title = subject_id + ' - ' + group
+    output_file = os.path.join(set_figure_base_dir('connectivity'),
+                               '_'.join([suffix, 'connectome', group,
+                                         str(session), subject_id]))
+    plt.figure(figsize=(10, 20), dpi=90)
+    if save:    
+        plot_connectome(pc, roi_coords, edge_threshold='85%', title=title,
+                        output_file=output_file)
+    else:
+        plot_connectome(pc, roi_coords, edge_threshold='85%', title=title)
+    
+def compute_pearson_connectivity(subject_id, group, session='func1',
+                                 preprocessing_folder='pipeline_1',
+                                 plot=True, save=True):
+    """Returns Pearson correlation coefficient for a subject_id
+    """    
+    # load timeseries
+    ts = load_dynacomp_roi_timeseries(subject_id, session=session,
+                                      preprocessing_folder=preprocessing_folder)
+    # load rois
+    roi_names, roi_coords = load_roi_names_and_coords(subject_id)
+
+    # pearson correlation
+    pc = np.corrcoef(ts.T)
+
+    if plot:
+        plot_connectivity_matrix(subject_id, group, pc,
+                                 roi_names, 'pearson_corr', save)
+        plot_connectivity_glassbrain(subject_id, group, pc,
+                                     roi_coords, 'pearson_corr', save)
+
+    return pc, roi_names, roi_coords
 
 
 def compute_graph_lasso_covariance(subject_id, group, session='func1',
@@ -54,32 +90,55 @@ def compute_graph_lasso_covariance(subject_id, group, session='func1',
     gl.fit(ts)
     pc = gl.covariance_
     if plot:
-        plot_connectivity_matrix(subject_id, group, pc, roi_names, 'gl_covariance')
-        plot_connectivity_glassbrain(subject_id, group, pc, roi_coords, 'gl_covariance')
+        plot_connectivity_matrix(subject_id, group, gl.covariance_,
+                                 roi_names, 'gl_covariance', save)
+        plot_connectivity_matrix(subject_id, group, gl.precision_,
+                                 roi_names, 'gl_precision', save)
+        sparsity = (gl.precision_ == 0)
+        plot_connectivity_matrix(subject_id, group, sparsity,
+                                 roi_names, 'gsc_sparsity', save)
+
+
+        plot_connectivity_glassbrain(subject_id, group, gl.covariance_,
+                                     roi_coords, 'gl_covariance', save)
 
     return pc, roi_names, roi_coords
 
-    
-def compute_pearson_connectivity(subject_id, group, session='func1',
-                                 preprocessing_folder='pipeline_1',
-                                 plot=True, save=True):
-    """Returns Pearson correlation coefficient for a subject_id
-    """
-    
-    # load timeseries
-    ts = load_dynacomp_roi_timeseries(subject_id, session=session,
-                                      preprocessing_folder=preprocessing_folder)
-    # load rois
-    roi_names, roi_coords = load_roi_names_and_coords(subject_id)
 
-    # pearson correlation    
-    pc = np.corrcoef(ts.T)
+def compute_group_sparse_covariance(dataset, session='func1',
+                                    preprocessing_folder='pipeline_1',
+                                    plot=True, save=True):
+    """Returns Group sparse covariance for all subjects
+    """
+    ts = []
+    for subject_id in dataset.subjects:
+        ts.append(load_dynacomp_roi_timeseries(subject_id, session=session,\
+                  preprocessing_folder=preprocessing_folder))
+    gsc = GroupSparseCovarianceCV(verbose=2)
+    gsc.fit(ts)
 
     if plot:
-        plot_connectivity_matrix(subject_id, group, pc, roi_names, 'pearson_corr')
-        plot_connectivity_glassbrain(subject_id, group, pc, roi_coords, 'pearson_corr')
+        for i in range(len(dataset.subjects)):
+            # load rois
+            roi_names, roi_coords = load_roi_names_and_coords(dataset.subjects[i])
 
-    return pc, roi_names, roi_coords
+            plot_connectivity_matrix(dataset.subjects[i], dataset.group[i],
+                                     gsc.covariances_[..., i],
+                                     roi_names, 'gsc_covariance', save)
+
+            plot_connectivity_matrix(dataset.subjects[i], dataset.group[i],
+                                     gsc.precisions_[..., i],
+                                     roi_names, 'gsc_precision', save)
+
+            sparsity = (gsc.precisions_[..., i] == 0)
+            plot_connectivity_matrix(dataset.subjects[i], dataset.group[i],
+                                     sparsity,
+                                     roi_names, 'gsc_sparsity', save)
+
+            plot_connectivity_glassbrain(dataset.subjects[i], dataset.group[i],
+                                         gsc.covariances_[..., i],
+                                         roi_coords, 'gsc_covariance', save)
+    return gsc
 
 
 
@@ -91,3 +150,5 @@ for i in range(len(dataset.subjects)):
     pc, roi_names, roi_coords = compute_pearson_connectivity(dataset.subjects[i],
                                                              dataset.group[i])    
     compute_graph_lasso_covariance(dataset.subjects[i], dataset.group[i])
+
+compute_group_sparse_covariance(dataset)
