4ddb197b93d91f63c7254ebb2436ff77c003479d
Mehdi Rahim <mehdi.rahim@cea.fr>
2015-04-10 09:38:18 +0200

    refactor signal extraction

--- extract_signals_from_rois.py
+++ extract_signals_from_rois.py
@@ -8,46 +8,36 @@ Created on Fri Mar 27 16:39:01 2015
 """
 import os, time
 import numpy as np
-import nibabel as nib
 from loader import load_dynacomp, dict_to_list
 from nilearn.input_data import NiftiMapsMasker
 
-dataset = load_dynacomp()
 
-for i in range(len(dataset.subjects)):
-    tic = time.clock()
-    output_path, _ = os.path.split(dataset.func1[i])
-#    if os.path.isfile(os.path.join(output_path, 'func1_rois_no_filter.npy')):        
-    print i, dataset.subjects[i]
-    maps_img = dict_to_list(dataset.rois[i])
-    # add mask, filtering and detrending
-    masker = NiftiMapsMasker(maps_img=maps_img, mask_img=dataset.mask,
-                             low_pass=None,
-                             high_pass=None,
-                             t_r=1.05,
-                             detrend=True,
-                             standardize=False,
-                             resampling_target='data',
-                             memory_level=0,
-                             verbose=5)
-    output_path, _ = os.path.split(dataset.func1[i])
-    x = masker.fit_transform(dataset.func1[i])
-    np.save(os.path.join(output_path, 'func1_rois_no_filter') , x)
-#    if os.path.isfile(os.path.join(output_path, 'func2_rois_no_filter.npy')):
-    print i, dataset.subjects[i]
-    maps_img = dict_to_list(dataset.rois[i])
-    # add mask, filtering and detrending
-    masker = NiftiMapsMasker(maps_img=maps_img, mask_img=dataset.mask,
-                             low_pass=None,
-                             high_pass=None,
-                             t_r=1.05,
-                             detrend=True,
-                             standardize=False,
-                             resampling_target='data',
-                             memory_level=0,
-                             verbose=5)
+dataset = load_dynacomp(preprocessing_folder='pipeline_2',
+                        prefix='resampled_wr')
 
-    x = masker.fit_transform(dataset.func2[i])
-    np.save(os.path.join(output_path, 'func2_rois_no_filter') , x)
-    toc = time.clock()
-    print toc - tic
+# func1, func2
+for idx, func in enumerate([dataset.func1, dataset.func2]):
+    # all the subjects
+    for i in range(len(dataset.subjects)):
+        tic = time.clock()
+        output_path, _ = os.path.split(func[i])
+        print dataset.subjects[i]
+        maps_img = dict_to_list(dataset.rois[i])
+        # add mask, smoothing, filtering and detrending
+        masker = NiftiMapsMasker(maps_img=maps_img, mask_img=dataset.mask,
+                                 low_pass=.1,
+                                 high_pass=.01,
+                                 smoothing_fwhm=6.,
+                                 t_r=1.05,
+                                 detrend=True,
+                                 standardize=False,
+                                 resampling_target='data',
+                                 memory_level=0,
+                                 verbose=5)
+        output_path, _ = os.path.split(func[i])
+        # extract the signal to x
+        x = masker.fit_transform(func[i])
+        np.save(os.path.join(output_path,
+                             'func' + str(idx+1) + '_rois_filter'), x)
+        toc = time.clock()
+        print toc - tic
